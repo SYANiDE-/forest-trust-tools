d5007ce0899228133708eedbcbccbf66e1908ba9
SYANiDE- <chase.hatch.business@gmail.com>
2025-06-23 12:32:22 -0700

    ftinfo.py cleanup, CLI arguments

--- ftinfo.py
+++ ftinfo.py
@@ -5,14 +5,36 @@
 # Author:
 #  Dirk-jan Mollema (@_dirkjan)
 #
-import binascii
+import binascii, argparse, sys
 from impacket.structure import Structure
 from impacket.ldap.ldaptypes import LDAP_SID
 
-# Paste data from ADUC here for analysis
+# Example data from ADUC here for analysis
+EXAMPLE_STRUCT = (
+    "01 00 00 00 04 00 00 00 1B 00 00 00 00 00 00 00 C2 72 D5 01 4E D9 12 F5 00 "
+    "0A 00 00 00 64 6F 6D 64 2E 6C 6F 63 61 6C 48 00 00 00 00 00 00 00 47 76 D5 "
+    "01 BF EC A6 77 02 18 00 00 00 01 04 00 00 00 00 00 05 15 00 00 00 9A D8 33 "
+    "B7 8C FD 3E 25 EA 68 12 19 11 00 00 00 73 75 62 74 77 6F 2E 64 6F 6D 64 2E "
+    "6C 6F 63 61 6C 06 00 00 00 53 55 42 54 57 4F 42 00 00 00 00 00 00 00 D2 72 "
+    "D5 01 BF 51 8D 5A 02 18 00 00 00 01 04 00 00 00 00 00 05 15 00 00 00 35 E3 "
+    "41 33 E4 64 F7 57 72 08 79 06 0E 00 00 00 73 75 62 2E 64 6F 6D 64 2E 6C 6F "
+    "63 61 6C 03 00 00 00 53 55 42 3F 00 00 00 00 00 00 00 C2 72 D5 01 4E D9 12 "
+    "F5 02 18 00 00 00 01 04 00 00 00 00 00 05 15 00 00 00 50 58 04 7A 06 B4 A7 "
+    "5C 1D 99 68 A2 0A 00 00 00 64 6F 6D 64 2E 6C 6F 63 61 6C 04 00 00 00 64 6F "
+    "6D 64"
+)
 
-data='01 00 00 00 04 00 00 00 1B 00 00 00 00 00 00 00 C2 72 D5 01 4E D9 12 F5 00 0A 00 00 00 64 6F 6D 64 2E 6C 6F 63 61 6C 48 00 00 00 00 00 00 00 47 76 D5 01 BF EC A6 77 02 18 00 00 00 01 04 00 00 00 00 00 05 15 00 00 00 9A D8 33 B7 8C FD 3E 25 EA 68 12 19 11 00 00 00 73 75 62 74 77 6F 2E 64 6F 6D 64 2E 6C 6F 63 61 6C 06 00 00 00 53 55 42 54 57 4F 42 00 00 00 00 00 00 00 D2 72 D5 01 BF 51 8D 5A 02 18 00 00 00 01 04 00 00 00 00 00 05 15 00 00 00 35 E3 41 33 E4 64 F7 57 72 08 79 06 0E 00 00 00 73 75 62 2E 64 6F 6D 64 2E 6C 6F 63 61 6C 03 00 00 00 53 55 42 3F 00 00 00 00 00 00 00 C2 72 D5 01 4E D9 12 F5 02 18 00 00 00 01 04 00 00 00 00 00 05 15 00 00 00 50 58 04 7A 06 B4 A7 5C 1D 99 68 A2 0A 00 00 00 64 6F 6D 64 2E 6C 6F 63 61 6C 04 00 00 00 64 6F 6D 64'
-data2 = binascii.unhexlify(data.replace(' ',''))
+def get_args():
+    parser = argparse.ArgumentParser(description="msDS-TrustForestTrustInfo struct parser")
+    meg = parser.add_mutually_exclusive_group(required=True)
+    meg.add_argument("-s", "--struct", type=str, default=None,help="The msDS-TrustForestTrustInfo struct to parse")
+    meg.add_argument("-e", "--example", action='store_true', help="Show an example struct for demo use")
+    ar,trash = parser.parse_known_args()
+    args = vars(ar)
+    if args['example']:
+        print(f"[+] Example msDs-TrustForestTrustInfo struct:\n{EXAMPLE_STRUCT}")
+        sys.exit()
+    return args
 
 
 class FOREST_TRUST_INFO_RECORD(Structure):
@@ -66,12 +88,20 @@ class FOREST_TRUST_INFO(Structure):
             self['Records'].append(FOREST_TRUST_INFO_RECORD(rdata))
             rdata = rdata[len(self['Records'][-1]):]
 
-fi = FOREST_TRUST_INFO(data2)
-fi.dump()
-for record in fi['Records']:
-    # record.dump()
-    try:
-        print('Domain %s has SID %s' % (record['Data']['DnsName'], record['Data']['Sid'].formatCanonical()))
-        print('Domain %s has SID %s' % (record['Data']['DnsName'], record['Data']['Sid'].getData()))
-    except KeyError:
-        pass
+def main():
+    args = get_args()
+    struct_dat = binascii.unhexlify(args['struct'].replace(' ',''))
+    fi = FOREST_TRUST_INFO(struct_dat)
+    fi.dump()
+    for record in fi['Records']:
+        # record.dump()
+        try:
+            dnsName = record['Data']['DnsName']
+            sid =     record['Data']['Sid']        
+            print(f'Domain {dnsName} has SID {sid.formatCanonical()}')
+            print(f'Domain {dnsName} has SID {sid.getData()}')
+        except KeyError:
+            pass
+
+if __name__=="__main__":
+    main()
